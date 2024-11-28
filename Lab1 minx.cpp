6d63fc2a0fd9ab14903532ca10990aa0c8ea030d
6704062610275 <s6704062610275@email.kmutnb.ac.th>
2024-11-28 14:59:29 +0700

    ข้อ2 แก้รอบ 2

--- Lab1 minx.cpp
+++ Lab1 minx.cpp
@@ -1,11 +1,18 @@
 #include<stdio.h>
 int main() {
-	int n1,n2;
+	int n1;
+	int n2;
+	int sum;
 	scanf("%d",&n1);
-	scanf("%d",&n2);	
+	scanf("%d",&n2);
+	
 	if (n1>n2) {
-		printf("n1 greater than n2");
+		printf("n1 greater than n2 \n");
+		sum= n1 - n2;
+		printf("%d",sum);
 	} else {
-		printf("n2 greater than n1");
-	}
+		printf("n2 greater than n1 \n");
+		sum= n2 - n1;
+		printf("%d",sum);
+	}	
 }
